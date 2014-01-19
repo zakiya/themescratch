f55784bc753c2763d3ab35cedc036e90510fb72f
Zakiya <info@zakiyadesigns.com>
2014-01-18 17:12:02 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// (function($) {
+
+//   $(document).ready(function() {
+
+//     // Erase Default Value if text is entered in form box.
+//     formEnterText($('#edit-name'), 'Enter UserName');
+//     formEnterText($('#edit-search-block-form--2'), 'Enter Search Terms');
+      
+//   });
+
+//   function formEnterText(selector, text) {
+
+//     selector.val(text);
+//     selector.focus(function() {
+//         if ($(this).val() == text)
+//          $(this).val('');
+//       }
+//     );
+
+//   }
+// })(jQuery);
---
+++ template.php
@@ -0,0 +1,10 @@
+<?
+
+// function scratchtheme_preprocess_html (&$vars) {
+//   drupal_add_css('http://fonts.googleapis.com/css?family=Chela+One', array('type' => 'external'));
+// }
+
+// function scratchtheme_preprocess_page (&$vars) {
+//   $vars['greeting'] = t('Hello, ') . $vars['user']->name;
+//   //dpm($vars);
+// }
